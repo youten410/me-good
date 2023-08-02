87c6affa9cd08b5d12cc51e79e0da2895d88bc36
Shun Sato <techregisteremail@gmail.com>
2023-08-03 01:39:36 +0900

    add : ルーターの作成

---
+++ lib/router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
+import 'package:me_good/screens/home_screen.dart';
+import 'package:me_good/screens/calendar_screen.dart';
+import 'package:me_good/screens/positive_feedback.dart';
+
+final GoRouter goRouter = GoRouter(
+  routes: [
+    GoRoute(
+        path: '/Home',
+        pageBuilder: (context, state) {
+          return MaterialPage(child: HomeScreen());
+        }),
+    GoRoute(
+        path: '/',
+        pageBuilder: (context, state) {
+          return MaterialPage(child: (CalendarScreen()));
+        }),
+    GoRoute(
+        path: '/positive-feedback',
+        pageBuilder: (context, state) {
+          return MaterialPage(child: (PositiveFeedback()));
+        }),
+  ],
+);

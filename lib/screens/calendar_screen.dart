f4ce03a68b842d2ccdd65c50eb9b08a5e0ba3bf3
Shun Sato <techregisteremail@gmail.com>
2023-08-04 01:17:50 +0900

    add : 一旦カレンダーを追加

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:table_calendar/src/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});
@@ -10,10 +13,12 @@ class CalendarScreen extends StatefulWidget {
 class _CalendarScreenState extends State<CalendarScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('カレンダー'),
-      ),
-    );
+    return  Center(
+          child: TableCalendar(
+            firstDay: DateTime.utc(2022, 4, 1),
+            lastDay: DateTime.utc(2025, 12, 31),
+            focusedDay: DateTime.utc(2022, 4, 1),
+          ),
+        );
   }
 }

2bf71e5332ed1ba15ae92e61f631208d55f36fc4
Shun Sato <techregisteremail@gmail.com>
2023-08-04 01:15:25 +0900

    add : feedbackページの作成

---
+++ lib/screens/feedback_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PositiveFeedback extends StatefulWidget {
+  const PositiveFeedback({super.key});
+
+  @override
+  State<PositiveFeedback> createState() => _PositiveFeedbackState();
+}
+
+class _PositiveFeedbackState extends State<PositiveFeedback> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Text('feedback')
+    );
+  }
+}

6e3bdc44b77e14bc4ba2f1763c72dd6b29e8b2ff
Shun Sato <techregisteremail@gmail.com>
2023-08-04 01:16:16 +0900

    update : 余計なルートを削除し、タブバービューを追加

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:me_good/screens/good_screen.dart';
+import 'package:me_good/screens/calendar_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,28 +10,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  @override
+  final _tab = <Tab> [
+    Tab( text:'Good', icon: Icon(Icons.thumb_up)),
+    Tab( text:'Calender', icon: Icon(Icons.calendar_month)),
+  ];
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('home'),
-      ),body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text('今日の自分にGood!'),
+    return DefaultTabController(
+      length: _tab.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Icon(Icons.logo_dev),
+          bottom: TabBar(
+            tabs: _tab,
           ),
-          Center(
-            child: IconButton(
-              icon: Icon(Icons.thumb_up), 
-              iconSize: 200,
-              color: const Color.fromARGB(255, 255, 207, 34),
-              onPressed: () {  
-                print('tpped');
-              },
-            ),
-          ),
-        ],
+        ),
+        body: TabBarView(
+            children: <Widget> [
+              GoodScreen(),
+              CalendarScreen()
+            ]
+        ),
       ),
     );
   }

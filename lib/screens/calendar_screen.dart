32f094d7cdcea795f7aabfad03cd0ee3c98eae58
Shun Sato <techregisteremail@gmail.com>
2023-08-05 22:21:18 +0900

    delete : 使っていないライブラリを削除

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
 class CalendarScreen extends StatefulWidget {
   const CalendarScreen({super.key});

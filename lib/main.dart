ccb7b64aa5473cb249b4be34a8b7f7d2639c3c0d
Shun Sato <techregisteremail@gmail.com>
2023-08-05 22:57:35 +0900

    add : firebase , firestore関係のパッケージ追加と連携

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:go_router/go_router.dart';
 import 'package:me_good/router.dart';
 import 'package:me_good/screens/good_screen.dart';
 import 'package:me_good/screens/calendar_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 

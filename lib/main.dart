330e4528c36258f1f6488528b3acc154fa4e3d13
Shun Sato <techregisteremail@gmail.com>
2023-08-06 02:47:52 +0900

    update : firebase initialize の追加

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 Future<void> main() async {
-  await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  /// Constructs a [MyApp]
   const MyApp({super.key});
 
   @override

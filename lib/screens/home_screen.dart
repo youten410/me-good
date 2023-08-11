de910c65672018629fd1ef40cb9ce0d7cb176433
Shun Sato <techregisteremail@gmail.com>
2023-08-11 17:29:12 +0900

    update : appbarの色とタブバーのインディケーターの下線の太さなどを変更

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,9 +10,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final _tab = <Tab> [
-    Tab( text:'いいね', icon: Icon(Icons.thumb_up)),
-    Tab( text:'カレンダー', icon: Icon(Icons.calendar_month)),
+  final _tab = <Tab>[
+    Tab(text: 'いいね', icon: Icon(Icons.thumb_up)),
+    Tab(text: 'カレンダー', icon: Icon(Icons.calendar_month)),
   ];
 
   Widget build(BuildContext context) {
@@ -20,17 +20,16 @@ class _HomeScreenState extends State<HomeScreen> {
       length: _tab.length,
       child: Scaffold(
         appBar: AppBar(
-          title: Icon(Icons.logo_dev),
+          toolbarHeight: 20,
+          backgroundColor: HSLColor.fromAHSL(1.0, 30, 1.0, 0.75).toColor(),
+          //shadowColor: Colors.black,
           bottom: TabBar(
+            indicatorColor: HSLColor.fromAHSL(1.0, 10, 1.0, 0.75).toColor(),
+            indicatorWeight: 5,
             tabs: _tab,
           ),
         ),
-        body: TabBarView(
-            children: <Widget> [
-              GoodScreen(),
-              CalendarScreen()
-            ]
-        ),
+        body: TabBarView(children: <Widget>[GoodScreen(), CalendarScreen()]),
       ),
     );
   }

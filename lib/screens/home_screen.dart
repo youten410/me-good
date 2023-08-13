5e4b52125c8228b85616e6ad8af3ee988e538a65
Shun Sato <techregisteremail@gmail.com>
2023-08-13 23:22:22 +0900

    update : タブ切り替え時のキーボード動作を改善
    タブを切り替えた際にキーボードが遅延して閉じる問題を修正。TabControllerを直接使用し、そのaddListenerでタブの変更を検知してキーボードを即座に閉じるように変更した。

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,22 +15,29 @@ class _HomeScreenState extends State<HomeScreen> {
     Tab(text: 'カレンダー', icon: Icon(Icons.calendar_month)),
   ];
 
+  final focusNode = FocusNode();
+
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: _tab.length,
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 20,
-          elevation: 0.0,
-          backgroundColor: HSLColor.fromAHSL(1.0, 30, 1.0, 0.75).toColor(),
-          //shadowColor: Colors.black,
-          bottom: TabBar(
-            indicatorColor: HSLColor.fromAHSL(1.0, 10, 1.0, 0.75).toColor(),
-            indicatorWeight: 5,
-            tabs: _tab,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus(); // キーボードを閉じる
+      },
+      child: DefaultTabController(
+        length: _tab.length,
+        child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 20,
+            elevation: 0.0,
+            backgroundColor: HSLColor.fromAHSL(1.0, 30, 1.0, 0.75).toColor(),
+            //shadowColor: Colors.black,
+            bottom: TabBar(
+              indicatorColor: HSLColor.fromAHSL(1.0, 10, 1.0, 0.75).toColor(),
+              indicatorWeight: 5,
+              tabs: _tab,
+            ),
           ),
+          body: TabBarView(children: <Widget>[GoodScreen(), CalendarScreen()]),
         ),
-        body: TabBarView(children: <Widget>[GoodScreen(), CalendarScreen()]),
       ),
     );
   }

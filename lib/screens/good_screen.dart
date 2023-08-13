7993f7f82580740dcb9b00c9896afdc718de1617
Shun Sato <techregisteremail@gmail.com>
2023-08-13 23:20:45 +0900

    update : GestureDetectorとSingleChildScrollViewでラップ

--- lib/screens/good_screen.dart
+++ lib/screens/good_screen.dart
@@ -44,231 +44,229 @@ class _GoodScreenState extends State<GoodScreen> {
     offset: const Offset(0, 2),
   );
 
+  final focusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: HSLColor.fromAHSL(1.0, 33, 1.0, 0.85).toColor(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Center(
-          //   child: Text(
-          //     'テスト',
-          //     style: TextStyle(
-          //       color: Colors.deepPurple,
-          //       fontSize: 30,
-          //     ),
-          //   ),
-          // ),
-          // SizedBox(
-          //   height: 50,
-          // ),
-          Column(
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus(); // キーボードを閉じる
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 200,
-                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  Text(
-                    "♡ $goodCount",
-                    style: TextStyle(fontSize: 20, color: Colors.orangeAccent),
-                  ),
-                ]),
+              SizedBox(
+                height: 80,
               ),
-              // いいねボタン
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _isElevated = !_isElevated;
-                    goodCount++;
-                  });
-                  // 1秒後に元の状態に戻す
-                  Future.delayed(Duration(milliseconds: 500), () {
-                    setState(() {
-                      _isElevated = true;
-                    });
-                  });
-                },
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    AnimatedContainer(
-                      duration: const Duration(
-                        milliseconds: 200,
-                      ),
-                      height: 150,
-                      width: 150,
-                      decoration: BoxDecoration(
-                        color: HSLColor.fromAHSL(1.0, 40, 1.0, 0.75)
-                            .toColor(), // 面の色
-                        borderRadius: BorderRadius.circular(100),
-                        boxShadow: _isElevated
-                            ? [
-                                const BoxShadow(
-                                  color: Colors.grey, //右下のシャドーの色
-                                  offset: Offset(3, 3),
-                                  blurRadius: 10,
-                                  spreadRadius: 1,
-                                ),
-                                const BoxShadow(
-                                  color: Colors.white, // 左上のシャドーの色
-                                  offset: Offset(-3, -3),
-                                  blurRadius: 10,
-                                  spreadRadius: 1,
-                                ),
-                              ]
-                            : [
-                                // フラットになりすぎないよう、小さなシャドウを残す
-                                const BoxShadow(
-                                  color: Colors.grey,
-                                  offset: Offset(1, 1),
-                                  blurRadius: 2,
-                                  spreadRadius: 1,
-                                ),
-                                const BoxShadow(
-                                  color: Colors.white,
-                                  offset: Offset(-1, -1),
-                                  blurRadius: 2,
-                                  spreadRadius: 1,
-                                ),
-                              ],
-                      ),
-                    ),
-                    Icon(
-                      Icons.favorite,
-                      color: Colors.deepOrangeAccent,
-                      size: 70,
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          // テキストフィールド
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-              // boxShadow: [
-              //   BoxShadow(
-              //       color: Colors.black12,
-              //       blurRadius: 10.0,
-              //       spreadRadius: 1.0,
-              //       offset: Offset(10, 10))
-              // ],
-            ),
-            child: GestureDetector(
-                onTap: () {
-                  FocusScope.of(context).unfocus(); // キーボードを閉じる
-                },
-                child: Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    height: MediaQuery.of(context).size.width * 0.4,
-                    alignment: Alignment.centerLeft,
-                    child: TextFormField(
-                        controller: _titleController,
-                        decoration: InputDecoration(
-                          hintText: 'コメント',
-                          hintStyle: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.w500),
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.only(left: 20.0),
+              Column(
+                children: [
+                  Container(
+                    width: 200,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "♡ $goodCount",
+                            style: TextStyle(
+                                fontSize: 20, color: Colors.orangeAccent),
+                          ),
+                        ]),
+                  ),
+                  // いいねボタン
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isElevated = !_isElevated;
+                        goodCount++;
+                      });
+                      // 1秒後に元の状態に戻す
+                      Future.delayed(Duration(milliseconds: 500), () {
+                        setState(() {
+                          _isElevated = true;
+                        });
+                      });
+                    },
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        AnimatedContainer(
+                          duration: const Duration(
+                            milliseconds: 200,
+                          ),
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: HSLColor.fromAHSL(1.0, 40, 1.0, 0.75)
+                                .toColor(), // 面の色
+                            borderRadius: BorderRadius.circular(100),
+                            boxShadow: _isElevated
+                                ? [
+                                    const BoxShadow(
+                                      color: Colors.grey, //右下のシャドーの色
+                                      offset: Offset(3, 3),
+                                      blurRadius: 10,
+                                      spreadRadius: 1,
+                                    ),
+                                    const BoxShadow(
+                                      color: Colors.white, // 左上のシャドーの色
+                                      offset: Offset(-3, -3),
+                                      blurRadius: 10,
+                                      spreadRadius: 1,
+                                    ),
+                                  ]
+                                : [
+                                    // フラットになりすぎないよう、小さなシャドウを残す
+                                    const BoxShadow(
+                                      color: Colors.grey,
+                                      offset: Offset(1, 1),
+                                      blurRadius: 2,
+                                      spreadRadius: 1,
+                                    ),
+                                    const BoxShadow(
+                                      color: Colors.white,
+                                      offset: Offset(-1, -1),
+                                      blurRadius: 2,
+                                      spreadRadius: 1,
+                                    ),
+                                  ],
+                          ),
                         ),
-                        maxLength: 100,
-                        maxLines: null,
-                        style: TextStyle(fontSize: 15),
-                        onChanged: (text) => setState(() {
-                              _titleCompleted = text.isNotEmpty;
-                              comment = _titleController.text.toString();
-                            })))),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Center(
-            child: Container(
-              width: 300,
-              child: PushableButton(
-                child: const Text(
-                  'きろく',
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.deepOrangeAccent,
+                          size: 70,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              // テキストフィールド
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                height: 60,
-                elevation: 8,
-                hslColor: const HSLColor.fromAHSL(1.0, 30, 1.0, 0.75),
-                // shadow: shadow,
-                onPressed: () {
-                  if (goodCount != 0 && comment.isNotEmpty) {
-                    DateTime now = DateTime.now();
-                    DateTime date = DateTime(now.year, now.month, now.day);
-                    String dateString =
-                        DateFormat('yyyy-MM-dd').format(date); // 日付を文字列に変換
-                    saveData(dateString, goodCount, comment);
-                    // モーダルの表示　バツボタンの位置は上に保持したい
-                    showModalBottomSheet(
-                        backgroundColor: Colors.white,
-                        context: context,
-                        isScrollControlled: true,
-                        builder: (BuildContext bc) {
-                          return Container(
-                            padding: EdgeInsets.only(top: 100.0),
-                            height: MediaQuery.of(context).size.height,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
+                child: GestureDetector(
+                    onTap: () {
+                      FocusScope.of(context).unfocus(); // キーボードを閉じる
+                    },
+                    child: Container(
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.width * 0.4,
+                        alignment: Alignment.centerLeft,
+                        child: TextFormField(
+                            focusNode: focusNode,
+                            controller: _titleController,
+                            decoration: InputDecoration(
+                              hintText: 'コメント',
+                              hintStyle: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w500),
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.only(left: 20.0),
+                            ),
+                            maxLength: 100,
+                            maxLines: null,
+                            style: TextStyle(fontSize: 15),
+                            onChanged: (text) => setState(() {
+                                  _titleCompleted = text.isNotEmpty;
+                                  comment = _titleController.text.toString();
+                                })))),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  child: PushableButton(
+                    child: const Text(
+                      'きろく',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    height: 60,
+                    elevation: 8,
+                    hslColor: const HSLColor.fromAHSL(1.0, 30, 1.0, 0.75),
+                    // shadow: shadow,
+                    onPressed: () {
+                      if (goodCount != 0 && comment.isNotEmpty) {
+                        DateTime now = DateTime.now();
+                        DateTime date = DateTime(now.year, now.month, now.day);
+                        String dateString =
+                            DateFormat('yyyy-MM-dd').format(date); // 日付を文字列に変換
+                        saveData(dateString, goodCount, comment);
+                        // モーダルの表示　バツボタンの位置は上に保持したい
+                        showModalBottomSheet(
+                            backgroundColor: Colors.white,
+                            context: context,
+                            isScrollControlled: true,
+                            builder: (BuildContext bc) {
+                              return Container(
+                                padding: EdgeInsets.only(top: 100.0),
+                                height: MediaQuery.of(context).size.height,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
                                   children: [
-                                    IconButton(
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                          setState(() {
-                                            resetData();
-                                          });
-                                        },
-                                        icon: Icon(Icons.close))
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        IconButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                              setState(() {
+                                                resetData();
+                                              });
+                                            },
+                                            icon: Icon(Icons.close))
+                                      ],
+                                    ),
+                                    Container(
+                                      color: Colors.white,
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            padding: EdgeInsets.only(
+                                                right: 20.0, left: 20.0),
+                                            child: Text(
+                                              '"$goodCount"いいね!\n素晴らしい!今日も自分を褒めてあげよう!',
+                                              style: TextStyle(
+                                                  fontSize: 30,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                          Container(
+                                              height: 500,
+                                              width: 300,
+                                              child: Image.asset(
+                                                  'assets/images/good_image.jpg'))
+                                        ],
+                                      ),
+                                    )
                                   ],
                                 ),
-                                Container(
-                                  color: Colors.white,
-                                  child: Column(
-                                    children: [
-                                      Container(
-                                        padding: EdgeInsets.only(
-                                            right: 20.0, left: 20.0),
-                                        child: Text(
-                                          '"$goodCount"いいね!\n素晴らしい!今日も自分を褒めてあげよう!',
-                                          style: TextStyle(
-                                              fontSize: 30,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                      Container(
-                                          height: 500,
-                                          width: 300,
-                                          child: Image.asset(
-                                              'assets/images/good_image.jpg'))
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          );
-                        });
-                  } else {
-                    // モーダル？か何かで表示（上から出るやつとか
-                    print("どちらかが不足");
-                  }
-                },
+                              );
+                            });
+                      } else {
+                        // モーダル？か何かで表示（上から出るやつとか
+                        print("どちらかが不足");
+                      }
+                    },
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }

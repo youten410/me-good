340c71ca5a54120f0eb7a22e1c7ccee9a8ac75d7
Shun Sato <techregisteremail@gmail.com>
2023-08-16 02:55:17 +0900

    update : ルートの変更

--- lib/router.dart
+++ lib/router.dart
@@ -5,11 +5,12 @@ import 'package:me_good/screens/good_screen.dart';
 import 'package:me_good/screens/calendar_screen.dart';
 import 'package:me_good/screens/home_screen.dart';
 import 'package:me_good/screens/feedback_screen.dart';
+import 'package:me_good/screens/login_page.dart';
 
 final GoRouter goRouter = GoRouter(
   routes: [
     GoRoute(
-        path: '/',
+        path: '/home',
         pageBuilder: (context, state) {
           return MaterialPage(child: HomeScreen());
         }),
@@ -17,6 +18,11 @@ final GoRouter goRouter = GoRouter(
         path: '/feedBack',
         pageBuilder: (context, state) {
           return MaterialPage(child: PositiveFeedback());
+        }),
+    GoRoute(
+        path: '/',
+        pageBuilder: (context, state) {
+          return MaterialPage(child: LoginPage());
         }),
   ],
 );
